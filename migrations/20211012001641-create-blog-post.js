7f8b542d6d11bc8aa838c39c68b58c5b46fc0316
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-12 04:21:33 -0300

    feat: :sparkles: creates and configures model and migration files for BlogPost

---
+++ migrations/20211012001641-create-blog-post.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  /*
+    Material consultado sobre allowNull e unique
+    https://sequelize.org/master/manual/validations-and-constraints.html
+  */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        /* allowNull: false, */
+        type: Sequelize.STRING
+      },
+      content: {
+        /* allowNull: false, */
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'published'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated'
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
---
+++ models/blogpost.js
@@ -0,0 +1,25 @@
+/*
+  Material consultado sobre allowNull e unique
+  https://sequelize.org/master/manual/validations-and-constraints.html
+*/
+const BlogPost = (sequelize, DataTypes) => {
+  const blogPost = sequelize.define('BlogPost', {
+    title: { type: DataTypes.STRING/* , allowNull: false  */ },
+    content: { type: DataTypes.STRING/* , allowNull: false  */ },
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+  }, {
+    updatedAt: 'updated',
+    createdAt: 'published',
+  });
+
+  blogPost.associate = (models) => {
+    blogPost.belongsTo(models.User, { as: 'user', foreignKey: 'userId' });
+
+/*     blogPost.belongsToMany(models.Category, {
+      as: 'categories', through: 'PostsCategories', foreignKey: 'postId', otherKey: 'categoryId' }); */
+  };
+
+  return blogPost;
+};
+
+module.exports = BlogPost;
