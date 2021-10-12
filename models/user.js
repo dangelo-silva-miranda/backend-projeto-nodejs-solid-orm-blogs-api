b2aac5a6571c971cff11030f8d2fe5fa9bf20fd3
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-12 04:22:34 -0300

    feat: :sparkles: create association between user and blogpost

--- models/user.js
+++ models/user.js
@@ -7,6 +7,13 @@ const User = (sequelize, DataTypes) => {
   }, {
     timestamps: false,
   });
+
+  user.associate = (models) => {
+    user.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blogposts',
+    });
+  };
+
   return user;
 };
 
