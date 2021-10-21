429ed24457b22f5a1cfb816e9b357e9fa7b3c863
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-21 10:16:29 -0300

    feat: :sparkles: checks if email exists

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,13 @@ const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
+const userEmailExists = async (email) => {
+  // Verifica se o e-mail já existe no DB
+  const emailDB = await User.findOne({ attributes: ['email'], where: { email } });
+  
+  return emailDB !== null;
+};
+
 const createToken = ({ id, email }) => {
   /* expiresIn aceita o tempo de forma bem descritiva: '7d' = 7 dias, '8h' = 8 horas. */
   const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
@@ -12,6 +19,11 @@ const createToken = ({ id, email }) => {
 };
 
 const createUser = async ({ displayName, email, password, image = '' }) => {
+  const emailExists = await userEmailExists(email);
+  if (emailExists) {
+    return { code: StatusCodes.CONFLICT, message: 'User already registered' };
+  }
+
   const { dataValues: { id } } = await User.create({ displayName, email, password, image });
     
   const token = createToken({ id, email });
