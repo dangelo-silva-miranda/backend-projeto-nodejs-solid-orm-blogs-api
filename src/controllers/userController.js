dbd5856ee62347846917c24f99acda9080c9647e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-21 10:03:08 -0300

    feat: :sparkles: implements createUser in userController

---
+++ src/controllers/userController.js
@@ -0,0 +1,15 @@
+const userService = require('../services/userService');
+
+const createUser = async (req, res) => {
+  const { code, message, token } = await userService.createUser(req.body);
+
+  if (!token) {
+    return res.status(code).json({ message });  
+  }
+  
+  return res.status(code).json({ token });
+};
+
+module.exports = {
+  createUser,
+};
