b4535e78b9269f1a541a83243de106d34709628d
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-21 10:08:53 -0300

    feat: :sparkles: adds userRouter with address '/user' in app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const { userRouter } = require('../routers/userRouter');
 
 const app = express();
 
@@ -7,14 +8,14 @@ const app = express();
   Material consultado sobre bodyParser.json vs bodyParser.urlencoded
   https://stackoverflow.com/questions/55558402/what-is-the-meaning-of-bodyparser-urlencoded-extended-true-and-bodypar
 */
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
 
-/* Todas as rotas com /users/<alguma-coisa> entram aqui e vão para o roteador */
-// app.use('/users', userRouter);
+/* Todas as rotas com /user/<alguma-coisa> entram aqui e vão para o roteador */
+app.use('/user', userRouter);
 
 module.exports = app;
