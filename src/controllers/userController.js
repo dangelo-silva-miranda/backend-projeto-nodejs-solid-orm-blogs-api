1671e2455f890bc57371322f84d359f4200f9101
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-22 00:24:56 -0300

    feat: :sparkles: implement findUserByPk in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,8 +26,19 @@ const findAllUsers = async (req, res) => {
   return res.status(code).json(users);
 };
 
+const findUserByPk = async (req, res) => {
+  const { code, message, user } = await userService.findUserByPk(req.params);
+
+  if (!user) {
+    return res.status(code).json({ message });  
+  }
+
+  return res.status(code).json(user);
+};
+
 module.exports = {
   createUser,
   loginUser,
   findAllUsers,
+  findUserByPk,
 };
