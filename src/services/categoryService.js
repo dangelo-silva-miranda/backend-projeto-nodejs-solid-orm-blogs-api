e88cff116d44f98bc5f8c3420b970822bef9e921
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-22 01:13:18 -0300

    feat: :sparkles: check name using categoryDataSchema and Joi

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -3,6 +3,12 @@ const { Category } = require('../models');
 const { categoryDataSchema } = require('./schemas');
 
 const createCategory = async ({ name }) => {
+  const { error } = categoryDataSchema.validate({ name });
+  if (error) { // error.isJoi indentifica se o erro foi do tipo Joi
+    const { message } = error.details[0];    
+    return { code: StatusCodes.BAD_REQUEST, message };
+  }
+  
   const { dataValues: category } = await Category.create({ name });
 
   return { code: StatusCodes.CREATED, category };
