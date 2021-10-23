3495009e43ee11c8db5f4066f07a67f04384ba30
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-10-23 01:13:00 -0300

    feat: :sparkles: implement createPost in postService

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,9 +1,40 @@
 const { StatusCodes } = require('http-status-codes');
-const { BlogPost } = require('../models');
+const { BlogPost, PostsCategory } = require('../models');
 // const { blogPostDataSchema } = require('./schemas');
 
 const createPost = async ({ title, content, userId, categoryIds }) => {
-  const { dataValues: post } = await BlogPost.create({ title, content, userId, categoryIds });
+  // const result = await BlogPost.create({ 
+  //   title, content, user: { id: userId }, categories: [categoryIds], 
+  // });
+
+  // const result = await BlogPost.create({ 
+  //   title, content, userId, 
+  // });
+  
+    // const result = await BlogPost.create({
+    //   title,
+    //   content,
+    //   userId, 
+    //   categories: categoryIds },
+    //   {
+    //     include: {
+    //       model: PostsCategories,
+    //       // through: 'PostsCategory',
+    //       as: 'categories',
+    //       attributes: ['categoryId'],
+    //     },
+    //   });
+
+    const { dataValues } = await BlogPost.create({ title, content, userId });
+    const post = dataValues;
+    delete post.updated;
+    delete post.published;
+
+    const { id: postId } = post;
+    
+    categoryIds.forEach(async (categoryId) => {
+      await PostsCategory.create({ postId, categoryId });      
+    });
 
   return { code: StatusCodes.CREATED, post };
 };
